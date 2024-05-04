6c9c8292757892adec2a1564dbd899d695101d9f
tjsiv <tjsivelsjr@gmail.com>
2024-05-04 12:20:28 -0700

    sending index and show data

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -6,11 +6,12 @@ const Bread = require('../models/bread.js');
 breads.get('/', (req, res) => {
     res.render('Index',
       {
-        breads: Bread
+        breads: Bread,
+        title: 'Index Page'
       }
     )
-  // res.send(Bread)
 })
+
 
 
 //Show
--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -1,19 +1,18 @@
 const React = require('react')
 
 function Default(html) {
-  return (
-    <html>
-    <head>
-      <title>Default</title>
-    </head>
-    <body>
-      <h1>HTML Rendered!</h1>
-      <div className="container">
+    return (
+      <html>
+      <head>
+        <title>{html.title || 'Default'}</title>
+      </head>
+      <body>
+        <h1>HTML Rendered!</h1>
         {html.children}
-      </div>
-    </body>
-    </html>
-  )
-}
+      </body>
+      </html>
+    )
+  }
+  
 
 module.exports = Default
