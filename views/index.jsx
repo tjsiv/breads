bbf72fef597d1133db3498ad2847d278fd7cd4a1
tjsiv <tjsivelsjr@gmail.com>
2024-05-06 21:11:07 -0700

    create and new reoutes complete

--- views/index.jsx
+++ views/index.jsx
@@ -5,6 +5,11 @@ function Index ({breads, title})  {
     return (
         <Default title={title}>
             <h2>Index Page</h2>
+            <div className="newButton">
+                <a href="/breads/new"><button>Add a new bread</button></a>
+            </div>
+
+
             {/* <p>I have {breads[0].name} bread!</p> */}
             {/* This is a JSX comment. */}
             <ul>
