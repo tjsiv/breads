4f3ad589e51f052424f2916c7e1879988d4745c8
tjsiv <tjsivelsjr@gmail.com>
2024-05-06 18:44:14 -0700

    sending index and show data

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -14,8 +14,16 @@ breads.get('/', (req, res) => {
 
 
 
-//Show
+// SHOW
 breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex]);
-});
+  if (Bread[req.params.arrayIndex]) {
+    res.render('Show', {
+      bread:Bread[req.params.arrayIndex]
+    })
+  } else {
+    res.send('404')
+  }
+})
+
+
 module.exports = breads;
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 // MIDDLEWARE
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
@@ -19,6 +20,11 @@ app.get('/', (req,res) =>{
 //BREADS
 const breadsController = require('./controllers/breads_controllers');
 app.use('/breads', breadsController);
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
 
 // LISTEN
 app.listen(PORT, () => {
