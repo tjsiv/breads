7fce7c23883c61c01787e03224113513e7e468ac
tjsiv <tjsivelsjr@gmail.com>
2024-05-08 19:50:51 -0700

    delete route complete

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -22,7 +22,8 @@ breads.get('/new', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
     })
   } else {
     res.render('404')
@@ -44,6 +45,11 @@ breads.post('/', (req, res) => {
   res.redirect('/breads')
 })
 
+// DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
+})
 
 
 module.exports = breads;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // DEPENDENCIES
+const methodOverride = require('method-override')
 const express = require('express');
 
 // CONFIGURATION
@@ -7,6 +8,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 // MIDDLEWARE
+app.use(methodOverride('_method'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.set('views', __dirname + '/views')
